61ed091371bc7d8edbe3c0fd362c71b08407ba51
RubensGJ <rubinho7.rr@gmail.com>
2025-04-14 10:29:57 -0400

    caminho do menu até o cod

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from conversores.conversor_moeda import converter_moeda
+from conversores.conversor_unidade import conversor_unidade
 
 def exibir_menu():
     print("\n=== MENU DE OPCOES ===")
     print("1 - Conversor de Moedas")
+    print("2 - Conversor de Unidades")
     print("0 - Sair")
     opcao = input("Digite sua opção: ")
     return opcao
@@ -12,6 +14,8 @@ def main():
         opcao = exibir_menu()
         if opcao == "1":
             converter_moeda()
+        elif opcao == "2":
+            conversor_unidade()
         elif opcao == "0":
             print("Encerrando o programa.")
             break
